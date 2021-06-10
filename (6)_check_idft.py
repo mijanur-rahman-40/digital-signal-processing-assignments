84f54a07fe91f969f6f9c1da828b5d7536a7aacf
mijanur-rahman-40 <mijanurrahman31416@gmail.com>
2021-06-10 12:07:00 +0600

    updated

--- (6)_check_idft.py
+++ (6)_check_idft.py
@@ -1,7 +1,49 @@
-import numpy as np
+import math
 from scipy.fftpack import fft, ifft
+from dft_5 import DFT
 
-x = [0, 1, 2, 3]
-X = fft(x, 4)
-print(X)
+
+def IDFT(dft_regular, dft_imaginary):
+    index_range_len = len(dft_regular)
+    idft_regular, idft_imaginary = [], []
+
+    for n in range(index_range_len):
+        idft_regular_value, idft_imaginary_value = 0.0, 0.0
+        for k in range(index_range_len):
+            v_re = dft_regular[k] \
+                * (math.cos((2 * math.pi / index_range_len) * k * n)) \
+                - dft_imaginary[k] \
+                * (math.sin((2 * math.pi / index_range_len) * k * n))
+            v_im = dft_regular[k] \
+                * (math.sin((2 * math.pi / index_range_len) * k * n)) \
+                + dft_imaginary[k] \
+                * (math.cos((2 * math.pi / index_range_len) * k * n))
+            idft_regular_value += v_re
+            idft_imaginary_value += v_im
+
+        idft_regular_value /= index_range_len
+        idft_imaginary_value /= index_range_len
+
+        idft_regular.append(round(idft_regular_value))
+        idft_imaginary.append(round(idft_imaginary_value))
+
+    return idft_regular, idft_imaginary
+
+num = input("Give the values of signal x(n): ")
+x = [int(i) for i in num.split()]
+dft_re_array, dft_im_array = DFT(x)
+
+
+idft_regular, idft_imaginary = IDFT(dft_re_array, dft_im_array)
+
+print("\nInverse Discreate Fourier Transform (without libray): \n")
+for i in range(len(idft_regular)):
+    print(f'X[{i}] : ({idft_regular[i]} {idft_imaginary[i]}j)')
+
+X = fft(x, len(x))
+print("\nInverse Discreate Fourier Transform (using libray):\n")
 print(ifft(X))
+
+print("\nAnswer is same for both cases\n")
+
+
--- dft_5.py
+++ dft_5.py
@@ -2,7 +2,7 @@
 import math
 
 
-def dft(x):
+def DFT(x):
     x_regular = x
     x_imaginary = []
     dft_regular, dft_imaginary = [], []
@@ -33,43 +33,11 @@ def dft(x):
     return dft_regular, dft_imaginary
 
 
-def idft(dft_regular, dft_imaginary):
-    index_range_len = len(dft_regular)
-    idft_regular, idft_imaginary = [], []
-
-    for n in range(index_range_len):
-        idft_regular_value, idft_imaginary_value = 0.0, 0.0
-        for k in range(index_range_len):
-            v_re = dft_regular[k] \
-                * (math.cos((2 * math.pi / index_range_len) * k * n)) \
-                - dft_imaginary[k] \
-                * (math.sin((2 * math.pi / index_range_len) * k * n))
-            v_im = dft_regular[k] \
-                * (math.sin((2 * math.pi / index_range_len) * k * n)) \
-                + dft_imaginary[k] \
-                * (math.cos((2 * math.pi / index_range_len) * k * n))
-            idft_regular_value += v_re
-            idft_imaginary_value += v_im
-
-        idft_regular_value /= index_range_len
-        idft_imaginary_value /= index_range_len
-
-        idft_regular.append(round(idft_regular_value))
-        idft_imaginary.append(round(idft_imaginary_value))
-
-    return idft_regular, idft_imaginary
-
-
 if __name__ == '__main__':
     num = input("Give the values of signal x(n): ")
     x = [int(i) for i in num.split()]
 
-    dft_re_array, dft_im_array = dft(x)
+    dft_re_array, dft_im_array = DFT(x)
     print("\nDiscreate Fourier Transform :")
     for i in range(len(dft_re_array)):
         print(f'X[{i}] : ({dft_re_array[i]} {dft_im_array[i]}j)')
-
-    idft_regular, idft_imaginary = idft(dft_re_array, dft_im_array)
-    print("\nInverse Discreate Fourier Transform :")
-    for i in range(len(idft_regular)):
-        print(f'X[{i}] : ({idft_regular[i]})')
